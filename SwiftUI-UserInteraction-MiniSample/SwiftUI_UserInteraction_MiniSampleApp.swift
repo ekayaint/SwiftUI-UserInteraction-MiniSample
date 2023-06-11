baace562627fa39e371fda2c06bd9adb03c5dbfd
ekayaint <ekayaint@gmail.com>
2023-06-11 14:21:42 +0300

    Initial Commit

---
+++ SwiftUI-UserInteraction-MiniSample/SwiftUI_UserInteraction_MiniSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserInteraction_MiniSampleApp.swift
+//  SwiftUI-UserInteraction-MiniSample
+//
+//  Created by ekayaint on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserInteraction_MiniSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
